ba59f265bd7b5b3f724b12d2af573e308715f490
Israel B. Lewis <lewisisrael6@gmail.com>
2023-12-26 09:06:46 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Israel on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
